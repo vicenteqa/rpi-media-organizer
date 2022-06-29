4b728c33e1d4009d27820000c5ba9638abf06f45
Vicente <58304150+vicenteqa@users.noreply.github.com>
2022-06-29 17:46:05 +0200

    Add files via upload
    initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const fse = require("fs-extra");
+const glob = require("glob");
+const TORRENT_FOLDER_PATH = "./torrent/";
+const MEDIA_FOLDER_PATH = "./media/";
+
+function moveTorrentsToMedia() {
+  if (
+    fs.existsSync(TORRENT_FOLDER_PATH) === true &&
+    fs.existsSync(MEDIA_FOLDER_PATH === true)
+  ) {
+    const torrentFolders = fs.readdirSync(TORRENT_FOLDER_PATH);
+
+    torrentFolders.map((folder) => {
+      const videoFilesInFolderAmount = countVideoFilesInFolder(folder);
+      const folderMediaType = getFolderMediaType(videoFilesInFolderAmount);
+
+      if (folderMediaType === "movie") moveMovie(folder);
+      else if (folderMediaType === "tv") moveTvShow(folder);
+    });
+  }
+}
+
+function moveMovie(folder) {
+  const movieFile = getMovieFile(folder);
+  const sourceMoviePath = TORRENT_FOLDER_PATH + folder + "/" + movieFile;
+  const destinationMoviePath = MEDIA_FOLDER_PATH + "/movies/" + movieFile;
+  fs.renameSync(sourceMoviePath, destinationMoviePath);
+  return fse.rmdirSync(TORRENT_FOLDER_PATH + folder);
+}
+
+function moveTvShow(folder) {
+  const sourceTvPath = TORRENT_FOLDER_PATH + folder + "/";
+  const destinationTvPath = MEDIA_FOLDER_PATH + "tv/" + folder + "/";
+  return fse.renameSync(sourceTvPath, destinationTvPath);
+}
+
+function getMovieFile(folder) {
+  const folderFiles = fs.readdirSync(TORRENT_FOLDER_PATH + folder);
+  const indexOfMovieFile = folderFiles
+    .map((file) => isVideoFile(file))
+    .indexOf(true);
+  return folderFiles[indexOfMovieFile];
+}
+
+function countVideoFilesInFolder(folder) {
+  const contentPath = TORRENT_FOLDER_PATH + folder + "/**/*";
+  const files = glob.sync(contentPath);
+
+  const amountOfVideoFilesInFolder = files
+    .map((file) => isVideoFile(file))
+    .filter((value) => value === true).length;
+  return amountOfVideoFilesInFolder;
+}
+
+function getFolderMediaType(amountOfVideoFilesInFolder) {
+  if (amountOfVideoFilesInFolder > 1) return "tv";
+  else if (amountOfVideoFilesInFolder === 1) return "movie";
+  else return "other";
+}
+
+function isVideoFile(file) {
+  return (
+    file.includes("mp4") === true ||
+    file.includes("avi") === true ||
+    file.includes("mkv") == true
+  );
+}
+
+moveTorrentsToMedia();
